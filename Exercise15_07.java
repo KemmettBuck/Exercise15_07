a740b8083dbb5f7402ef48a1c71b55d844fc1e90
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-13 14:07:27 -0400

    add main, and launch(args)

--- Exercise15_07.java
+++ Exercise15_07.java
@@ -41,4 +41,8 @@ public class Exercise15_07 extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
