48e12ed481fbfabc6d76fb3c654e69fa062efd3e
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-13 14:04:05 -0400

    use lambda expression for event response for mouse pressed and released

--- Exercise15_07.java
+++ Exercise15_07.java
@@ -16,9 +16,21 @@ public class Exercise15_07 extends Application {
     public void start(Stage primaryStage) {
 
         // Create circle with radius 100, default color white
-        Circle circle = new Circle(100);
+        Circle circle = new Circle(125);
         circle.setFill(Color.WHITE);
         circle.setStroke(Color.BLACK);
-        circle.setStrokeWidth(2);
+        circle.setStrokeWidth(3);
+
+        // When mouse is pressed, turn circle black
+        circle.setOnMousePressed(e -> {
+            circle.setFill(Color.BLACK);
+        });
+
+        // When mouse is released, turn white
+        circle.setOnMouseReleased(e -> {
+            circle.setFill(Color.WHITE);
+        });
+
+        // 
     }
 }
