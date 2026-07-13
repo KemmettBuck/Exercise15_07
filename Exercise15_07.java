f2d062838745a5b228fd2c72300f189fc3245f4e
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-13 13:56:38 -0400

    import javaFX features, create public class extending Application

--- Exercise15_07.java
+++ Exercise15_07.java
@@ -1,1 +1,16 @@
+/**
+ * Exercise15_07
+ * Display circle that turns black when mouse is pressed
+ * and white when mouse is released
+ */
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class Exercise15_07 extends Application {
+    
+}
